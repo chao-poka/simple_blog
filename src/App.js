c8966f381cfa81ced08674562c86302cb215979e
skomarovski <mrpowerman551@gmail.com>
2022-10-22 09:22:30 +0600

    complete app structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import StyledApp from './components/styled/App.styled'
+import GlobalStyle from './components/styled/GlobalStyles'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme } from './themes'
+import { BrowserRouter } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyle />
+        <StyledApp>Hello World</StyledApp>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
